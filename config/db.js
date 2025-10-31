3903fe30439f8d342bd094e88b5a980b2874b966
Caike Batista <caike14@gmail.com>
2025-10-30 23:16:53 -0300

    Atualizar o db.js
    Atualizei as variáveis de ambiente de conexão do banco.

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER || 'root',
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3302,
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'projetoQuiz',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT, parseInt(process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   });
    
   connection.connect((err) => {
@@ -17,5 +17,5 @@ const connection = mysql.createConnection({
    console.log('Conectado como ID ' + connection.threadID);
   });
    
-  module.exports = connection.promise ();
+  module.exports = connection;
    
